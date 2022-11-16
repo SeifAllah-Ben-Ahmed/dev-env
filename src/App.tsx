b53668622c74f86240430ec1a2bcb123f98b11d3
SeifAllah-Ben-Ahmed <benahmed.seifallah@gmail.com>
2022-11-16 15:44:48 +0100

    Safely Handling Untrusted Code Execution

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
+
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -15,10 +16,9 @@ const App = () => {
       wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
-
   const onClick = async () => {
     if (!ref.current) return;
-
+    iframe.current.srcdoc = html;
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -30,13 +30,33 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
 
   useEffect(() => {
     startService();
   }, []);
-
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event)=>{
+            try{
+              eval(event.data)
+            }catch (error) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color:red;"> <h4>Runtime Error:</h4> ' + error + '</div>'
+              console.error(error)
+            }
+          } ,false)
+        </script>
+      </body>
+    </html>
+`;
+  // 106 COMPLETED!
   return (
     <div>
       <textarea
@@ -47,7 +67,13 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+
+      <iframe
+        ref={iframe}
+        title="Stand-alone Development Environment"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
